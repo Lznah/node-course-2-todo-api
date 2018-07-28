68ac333baca584ee18735ce952b791c506be2af7
Lznah <hanzlpe@icloud.com>
2018-07-29 00:21:13 +0200

    Add playground mongoose queries

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,29 @@
+const {ObjectId} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/Todo');
+
+var id = '5b5ce7da0a14222f8ec99f951';
+
+if(!ObjectId.isValid(id)) {
+  console.log('ID not valid');
+}
+
+// Todo.find({
+//   _id: id
+// }).then((todos)=> {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo)=> {
+//   console.log('Todo', todo);
+// });
+
+Todo.findById(id).then((todo) => {
+  if(!todo) {
+    return console.log('Id not found');
+  }
+  console.log('Todo By Id', todo);
+}).catch((e) => console.log(e));
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if(err) {
+    return console.log('Unable to connect to db');
+  }
+  console.log('Connected to db');
+});
 
 module.exports = {mongoose};
