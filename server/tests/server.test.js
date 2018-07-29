a9c6002cab1a71513b6e193dc9f6e93943eb39f3
Lznah <hanzlpe@icloud.com>
2018-07-29 17:32:13 +0200

    Test the DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -106,3 +106,46 @@ describe('GET /todos/:id', () => {
       .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toBeFalsy();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+
+  it('should return 404 if todo not found', (done) => {
+    var newId = new ObjectID();
+    request(app)
+      .delete(`/todos/${newId.toHexString()}`)
+      .expect(404)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+
+  it('should return 404 if objectID is invalid', (done) => {
+    var wrongId = 123;
+    request(app)
+      .delete(`/todos/${wrongId}`)
+      .expect(404)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
