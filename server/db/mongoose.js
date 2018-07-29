e95cfd04c740d34bfb38eb18aea8d35d44bc2e74
Lznah <hanzlpe@icloud.com>
2018-07-29 16:03:37 +0200

    Updated mongoose.js workaround

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', (err, db) => {
+
+var mongoURI = process.env.USER == "petrhanzl" ? 'mongodb://localhost:27017/TodoApp' : 'mongodb://<root>:<tajneheslo123>@ds259111.mlab.com:59111/todoapp';
+
+mongoose.connect(mongoURI, (err, db) => {
   if(err) {
     return console.log('Unable to connect to db');
   }
